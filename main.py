dfe3c9018f32e826f8f10e075c7050261a503103
Yisus <yisusMurci@.com>
2024-07-19 12:00:23 -0500

    Funciones de seleeción -Se crearon las funciones para seleccionar las columnas a marcar -Se puede iniciar el juego de a dos personas, aún esta pendiente la funcion para obtener ganador o un empate

--- main.py
+++ main.py
@@ -17,10 +17,32 @@ def crearTablero():
 def mostrarTablero(tablero):
     for i in range(len(tablero)):
         for value in tablero[i]: #Iterar por cada fila
-            print("x" if value == -1 else "o" if value == 1 else " ", end=" ")
+            print("x" if value == -1 else "o" if value == 1 else "-", end="|")
         print()#Salto de línea
     for i in range(len(tablero)):
         print(i+1, end=" ")#Para enumerar casillas
+    print()
+
+def pedirCasillaAMarcar(tablero):
+    casillaValida = False
+    posicion = input("Ingrese la casilla a marcar: ")
+    while not casillaValida:
+        if posicion in "123456" and len(posicion) == 1:
+            posicion = int(posicion) -1
+            for i in range(6): #Verificar que la casilla no este llena
+                if tablero[i][posicion] == 0:
+                    casillaValida = True
+                    break
+        if not casillaValida:
+            posicion = input("Ingrese una casilla valida, intente nuevamente: ")
+    return posicion
+
+def marcarTablero(tablero, posicion, jugador):
+    for i in range(5, -1, -1):
+        if tablero[i][posicion] == 0: #Revisar que la columna este vacia
+            tablero[i][posicion] = jugador
+            break
+    return tablero
 
 print("Bienvenido, jugemos conecta cuatro")
 system("pause")
@@ -36,6 +58,12 @@ while continuar:
     match(opcion):
         case 1:
             print("Jugar con dos personas")
+            jugador = 1
+            while 0 in tablero[0]:
+                casilla = pedirCasillaAMarcar(tablero)
+                tablero = marcarTablero(tablero, casilla, jugador)
+                mostrarTablero(tablero)
+                jugador*=-1
         case 2:
             print("juego individual")
         case 0:
