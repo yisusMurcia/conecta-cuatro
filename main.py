ebfc8cde70d4d002b8f2654b05322921dc4dd7d1
Yisus <yisusMurci@.com>
2024-07-22 09:08:54 -0500

    Correción -Se corrigió el error causado al no ingresar un valor en el menú de opciones

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from os import system
 
 def leerOpcion():
     opcion = input("Ingrese la opción a jugar: ")
-    while opcion not in "012":
-        opcion = input("ingrese una opción valida")
+    while opcion not in "012" or opcion == '':
+        opcion = input("ingrese una opción valida, intente otra vez: ")
     return int(opcion)
 
 def crearTablero():
-    tablero = [[ 0 for i in range(6)] for i in range(6)]
+    tablero = [[ 0 for i in range(7)] for i in range(6)]
     return tablero
 
 def mostrarTablero(tablero):
@@ -19,7 +19,7 @@ def mostrarTablero(tablero):
         for value in tablero[i]: #Iterar por cada fila
             print("x" if value == -1 else "o" if value == 1 else "-", end="|")
         print()#Salto de línea
-    for i in range(len(tablero)):
+    for i in range(len(tablero[0])):
         print(i+1, end=" ")#Para enumerar casillas
     print()
 
@@ -27,7 +27,7 @@ def pedirCasillaAMarcar(tablero):
     casillaValida = False
     posicion = input("Ingrese la casilla a marcar: ")
     while not casillaValida:
-        if posicion in "123456" and len(posicion) == 1:
+        if posicion in "1234567" and len(posicion) == 1:
             posicion = int(posicion) -1
             for i in range(6): #Verificar que la casilla no este llena
                 if tablero[i][posicion] == 0:
@@ -44,6 +44,34 @@ def marcarTablero(tablero, posicion, jugador):
             break
     return tablero
 
+def empate(tablero): 
+    empate = True
+    for columna in tablero:
+        if 0 in columna:
+            empate = False
+            break
+    return empate
+
+def victoria(tablero):
+    ganador = None
+    for i in range(len(tablero)):
+        if tablero[i][3] != 0:#la cuarta posición de cada fila siempre es necesaria para una línea horizontal
+            ganador = tablero[i][3]
+            posicion = 3
+            cantidad = 0
+
+            while posicion >= 0 and tablero[i][posicion] == ganador: #Buscar desde donde empieza la línea horizontal
+                posicion -= 1
+            posicion += 1 #Restablecer valor donde empieza el patron
+
+            while cantidad < 3 and tablero[i][posicion] == ganador:
+                posicion += 1
+                cantidad += 1
+            if cantidad == 4:
+                return ganador
+            else:
+                ganador = None
+    return ganador
 print("Bienvenido, jugemos conecta cuatro")
 system("pause")
 system("cls")
@@ -59,11 +87,16 @@ while continuar:
         case 1:
             print("Jugar con dos personas")
             jugador = 1
-            while 0 in tablero[0]:
+            while not empate(tablero) and victoria(tablero) == None:
                 casilla = pedirCasillaAMarcar(tablero)
                 tablero = marcarTablero(tablero, casilla, jugador)
                 mostrarTablero(tablero)
                 jugador*=-1
+            if empate(tablero):
+                print("Empate")
+            else:
+                print("victoria")
+
         case 2:
             print("juego individual")
         case 0:
